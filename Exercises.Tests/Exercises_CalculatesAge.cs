7ac7c065b7383ba92914bd2a21f7184d6f284c2c
Vanessa Denny <vanessaidenny@hotmail.com>
2020-10-29 11:08:10 -0300

    refactor: Remove redundant lines

--- Exercises.Tests/Exercises_CalculatesAge.cs
+++ Exercises.Tests/Exercises_CalculatesAge.cs
@@ -8,28 +8,22 @@ namespace Exercises.UnitTests
         [Fact]        
         public void CalculatesAge_Input365_Return1Year()
         {
-            DateTime test = DateTime.Today.Subtract(TimeSpan.FromDays(365.2425));
-            int result = Exercise.CalculatesAge(test);
-
-            Assert.True(result==1);
+            DateTime test = DateTime.Today.Subtract(TimeSpan.FromDays(365.2425));       
+            Assert.Equal(1, Exercise.CalculatesAge(test));
         }
 
         [Fact]
         public void CalculatesAge_Input364_Return0Year()
         {
             DateTime test = DateTime.Today.Subtract(TimeSpan.FromDays(364));
-            int result = Exercise.CalculatesAge(test);
-
-            Assert.True(result==0);
+            Assert.Equal(0, Exercise.CalculatesAge(test));
         }
 
         [Fact]
         public void CalculatesAge_InputNegativeDay_Return0()
         {
-            DateTime test = DateTime.Today.AddDays(-1);
-            int result = Exercise.CalculatesAge(test);
-
-            Assert.True(result==0);
+            DateTime test = DateTime.Today.AddDays(1);
+            Assert.Equal(0, Exercise.CalculatesAge(test));
         }
     }
 }
